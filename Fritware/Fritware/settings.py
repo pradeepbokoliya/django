08cb19cde3f292aa1e68b4f9e06bdc43fd270ad7
pradeepbokoliya <pradeepbokoliya@gmail.com>
2025-09-01 10:47:10 +0530

    db change

--- Fritware/Fritware/settings.py
+++ Fritware/Fritware/settings.py
@@ -87,13 +87,21 @@ WSGI_APPLICATION = 'Fritware.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         # ...
+#         "OPTIONS": {
+#             "init_command": "PRAGMA synchronous=3; PRAGMA cache_size=2000;",
+#         },
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        # ...
-        "OPTIONS": {
-            "init_command": "PRAGMA synchronous=3; PRAGMA cache_size=2000;",
-        },
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

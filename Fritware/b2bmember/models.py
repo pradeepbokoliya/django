95b93960aea64ed7de39dec6111332db61c284c4
pradeepbokoliya <pradeepbokoliya@gmail.com>
2025-09-13 09:25:03 +0530

    Window pc

--- Fritware/b2bmember/models.py
+++ Fritware/b2bmember/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import random
 
-# Create your models here.
+
+class memberSignup(models.Model):
+    user = models.CharField(max_length=100)
+    name = models.CharField(max_length=60)
+    email = models.EmailField(max_length=80)
+    password = models.CharField()
+    
--- Fritware/b2bmember/views.py
+++ Fritware/b2bmember/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from b2bmember.models import memberSignup
+import random
 
 # Create your views here.
 
-def member_login(request):
-    # third paramiter must be dict in rander function
-    pagedata = {'title':'My Member Login'}
+# def member_login(request):
+#     # third paramiter must be dict in rander function
+#     pagedata = {'title':'My Member Login'}
     
-    return render(request, "b2bmember/login.html", pagedata)
+#     return render(request, "b2bmember/login.html", pagedata)
 
 
 
@@ -16,3 +19,23 @@ def dt(request):
 
 def rt(request):
     return render(request, "b2bmember/rtlogin.html")
+
+
+def member_login(request):
+    pagedata = {'title':'My Member Login'}
+    if request.method=="POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        raw_password = request.POST.get('raw_password')
+
+        data = User.objects.create_user(
+            username = User(username = "fdsfsd"),
+        
+            email = email,
+            password=raw_password,
+            
+        )
+        
+        User.save()
+        
+    return render(request,"b2bmember/login.html", pagedata)
